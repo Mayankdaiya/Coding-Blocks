07d6acbd95ba2b1fa1b8e8732d6137175a7c90d6
Mayank Daiya <mayankdaiya03@gmail.com>
2022-09-22 15:22:27 +0530

    Recursion

---
+++ PrintSubsequence.java
@@ -0,0 +1,18 @@
+class PrintSubsequence{
+    //print subsequence
+    public static void subsequence(String str, String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        char ch=str.charAt(0);
+        subsequence(str.substring(1),ans);
+        subsequence(str.substring(1),ans+ch);
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        subsequence(str,"");
+    }
+}
