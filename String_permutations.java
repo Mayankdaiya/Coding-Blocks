b9762373fff7ecb64345adf769edd7e567a67bbe
Mayank Daiya <mayankdaiya03@gmail.com>
2022-09-28 21:08:33 +0530

    Recursion

---
+++ String_permutations.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class String_permutations{
+    public  static  void  print_string_permutation(String str,String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for(int j=0;j<str.length();j++){
+            char a=str.charAt(j);
+            String s1=str.substring(0,j);
+            String s2=str.substring(j+1);
+            print_string_permutation(s1+s2,ans+a);
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        print_string_permutation(str,"");
+    }
+}
