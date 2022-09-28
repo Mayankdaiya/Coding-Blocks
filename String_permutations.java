cc9a4e68e68cdaf4fd205413778473e7b3161123
Mayank Daiya <mayankdaiya03@gmail.com>
2022-09-28 21:08:52 +0530

    Recursion

--- String_permutations.java
+++ String_permutations.java
@@ -12,7 +12,7 @@ class String_permutations{
             print_string_permutation(s1+s2,ans+a);
         }
     }
-
+    
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         String str=sc.next();
