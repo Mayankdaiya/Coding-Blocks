33c214dd213fa51501819af98a4e9e945365ebb3
Mayank Daiya <mayankdaiya03@gmail.com>
2022-10-04 22:13:12 +0530

    ArrayList

--- Array_List.java
+++ Array_List.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Array_List {
+    
     public static void main(String[] args) {
         ArrayList<Integer> list=new ArrayList<>();
         //add
