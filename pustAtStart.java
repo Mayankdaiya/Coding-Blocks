9d052572e4e41416c794a429f1199c134d1d86f4
Mayank Daiya <mayankdaiya03@gmail.com>
2022-11-24 15:28:36 +0530

    Stack

---
+++ pustAtStart.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class pustAtStart{
+    public static void main(String args[]){
+        Stack<Integer> s=new Stack<>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        System.out.println(s);
+        Insert(s,-2);
+        System.out.println(s);
+
+    }
+
+    public static void Insert(Stack<Integer> s,int item){
+        int t;
+        if(s.isEmpty()){
+            s.push(item);
+        }
+        else{
+            t=s.pop();
+            Insert(s,item);
+            s.push(t);
+            return;
+        }
+    }
+}
